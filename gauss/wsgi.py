b2e8c6891c33ee892f6252d3a0401dc417c6932e
Juanjo <jmar0269@gmail.com>
2016-09-01 19:45:15 +0200

    wsgi working in Apache

--- gauss/wsgi.py
+++ gauss/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/juanjo/django/gauss'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gauss.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gauss_educa.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "gauss.settings"
 
 application = get_wsgi_application()
