b82abf1837035876ffe4bd6378436a5c2402dd0c
Juanjo <jmar0269@gmail.com>
2016-09-06 23:18:22 +0200

    New wsgi.py

--- gauss/wsgi.py
+++ gauss/wsgi.py
@@ -21,6 +21,6 @@ if path not in sys.path:
 from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gauss.settings")
-os.environ["DJANGO_SETTINGS_MODULE"] = "gauss_project.gauss.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "gauss.settings"
 
 application = get_wsgi_application()
